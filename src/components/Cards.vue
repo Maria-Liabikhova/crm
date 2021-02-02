06a070a00b2396cfa11ea683310bffcecade8168
unknown <mashagi@yandex.ru>
2021-02-02 16:11:01 +0300

    refactor: unnecessary tags in cards component deleted

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,56 +1,50 @@
 <template>
-  <div>
-    <v-container fluid>
-      <v-row>
-        <v-col
-          cols="6"
-          xs="6"
-          sm="6"
-          md="4"
-          v-for="user in filteredRole"
-          :key="user.id"
-        >
-          <v-card class="mx-auto card" max-width="344" min-width="300">
-            <router-link class="no-decoration" :to="'/user/' + user.id">
-              <v-img :src="user.imgSrc" height="300px"></v-img>
-              <v-card-title>
-                <p class="card__subtitle">
-                  Name:
-                  <span class="card__name"
-                    >{{ user.name }} {{ user.surname }}</span
-                  >
-                </p>
-              </v-card-title>
-              <v-card-subtitle>
-                <p class="card__subtitle">
-                  Id:
-                  <span class="card__describe">{{ user.id }}</span>
-                </p>
-                <p class="card__subtitle">
-                  Gender:
-                  <span class="card__describe">{{ user.gender }}</span>
-                </p>
-                <p class="card__subtitle">
-                  Role: <span class="card__describe">{{ user.role }}</span>
-                </p>
-                <p class="card__subtitle">
-                  eMail:
-                  <span class="card__describe">{{ user.eMail }}</span>
-                </p>
-                <p class="card__subtitle">
-                  Nickname:
-                  <span class="card__describe">{{ user.nickname }}</span>
-                </p>
-                <p class="card__subtitle">
-                  Age: <span class="card__describe">{{ user.age }}</span>
-                </p>
-              </v-card-subtitle>
-            </router-link>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <v-row>
+    <v-col
+      cols="6"
+      xs="6"
+      sm="6"
+      md="4"
+      v-for="user in filteredRole"
+      :key="user.id"
+    >
+      <v-card class="mx-auto card" max-width="344" min-width="300">
+        <router-link class="no-decoration" :to="'/user/' + user.id">
+          <v-img :src="user.imgSrc" height="300px"></v-img>
+          <v-card-title>
+            <p class="card__subtitle">
+              Name:
+              <span class="card__name">{{ user.name }} {{ user.surname }}</span>
+            </p>
+          </v-card-title>
+          <v-card-subtitle>
+            <p class="card__subtitle">
+              Id:
+              <span class="card__describe">{{ user.id }}</span>
+            </p>
+            <p class="card__subtitle">
+              Gender:
+              <span class="card__describe">{{ user.gender }}</span>
+            </p>
+            <p class="card__subtitle">
+              Role: <span class="card__describe">{{ user.role }}</span>
+            </p>
+            <p class="card__subtitle">
+              eMail:
+              <span class="card__describe">{{ user.eMail }}</span>
+            </p>
+            <p class="card__subtitle">
+              Nickname:
+              <span class="card__describe">{{ user.nickname }}</span>
+            </p>
+            <p class="card__subtitle">
+              Age: <span class="card__describe">{{ user.age }}</span>
+            </p>
+          </v-card-subtitle>
+        </router-link>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
