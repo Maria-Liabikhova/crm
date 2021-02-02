70c052d300c7ba6fb63d5f8b84eaea39c5afa8a5
unknown <mashagi@yandex.ru>
2021-02-02 15:54:51 +0300

    refactor: cards with users in separate component

---
+++ src/components/Cards.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-row>
+        <v-col
+          cols="6"
+          xs="6"
+          sm="6"
+          md="4"
+          v-for="user in filteredRole"
+          :key="user.id"
+        >
+          <v-card class="mx-auto card" max-width="344" min-width="300">
+            <router-link class="no-decoration" :to="'/user/' + user.id">
+              <v-img :src="user.imgSrc" height="300px"></v-img>
+              <v-card-title>
+                <p class="card__subtitle">
+                  Name:
+                  <span class="card__name"
+                    >{{ user.name }} {{ user.surname }}</span
+                  >
+                </p>
+              </v-card-title>
+              <v-card-subtitle>
+                <p class="card__subtitle">
+                  Id:
+                  <span class="card__describe">{{ user.id }}</span>
+                </p>
+                <p class="card__subtitle">
+                  Gender:
+                  <span class="card__describe">{{ user.gender }}</span>
+                </p>
+                <p class="card__subtitle">
+                  Role: <span class="card__describe">{{ user.role }}</span>
+                </p>
+                <p class="card__subtitle">
+                  eMail:
+                  <span class="card__describe">{{ user.eMail }}</span>
+                </p>
+                <p class="card__subtitle">
+                  Nickname:
+                  <span class="card__describe">{{ user.nickname }}</span>
+                </p>
+                <p class="card__subtitle">
+                  Age: <span class="card__describe">{{ user.age }}</span>
+                </p>
+              </v-card-subtitle>
+            </router-link>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['users', 'filteredRole']
+}
+</script>
+
+<style scoped>
+.card__name {
+  color: black;
+  font-weight: bold;
+  font-size: 25px;
+  word-break: keep-all;
+}
+.card__subtitle {
+  font-weight: bold;
+  color: rgb(116, 116, 116);
+  font-size: 20px;
+  margin-bottom: 7px;
+}
+.card__describe {
+  color: black;
+  font-size: 16px;
+}
+.no-decoration {
+  text-decoration: none;
+}
+</style>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -67,52 +67,7 @@
         </v-col>
         <v-col cols="8" xs="8" sm="8" md="9">
           <v-row>
-            <v-col
-              cols="6"
-              xs="6"
-              sm="6"
-              md="4"
-              v-for="user in filteredRole"
-              :key="user.id"
-            >
-              <v-card class="mx-auto card" max-width="344">
-                <router-link class="no-decoration" :to="'/user/' + user.id">
-                  <v-img :src="user.imgSrc" height="300px"></v-img>
-                  <v-card-title>
-                    <p class="card__subtitle">
-                      Name:
-                      <span class="card__name"
-                        >{{ user.name }} {{ user.surname }}</span
-                      >
-                    </p>
-                  </v-card-title>
-                  <v-card-subtitle>
-                    <p class="card__subtitle">
-                      Id:
-                      <span class="card__describe">{{ user.id }}</span>
-                    </p>
-                    <p class="card__subtitle">
-                      Gender:
-                      <span class="card__describe">{{ user.gender }}</span>
-                    </p>
-                    <p class="card__subtitle">
-                      Role: <span class="card__describe">{{ user.role }}</span>
-                    </p>
-                    <p class="card__subtitle">
-                      eMail:
-                      <span class="card__describe">{{ user.eMail }}</span>
-                    </p>
-                    <p class="card__subtitle">
-                      Nickname:
-                      <span class="card__describe">{{ user.nickname }}</span>
-                    </p>
-                    <p class="card__subtitle">
-                      Age: <span class="card__describe">{{ user.age }}</span>
-                    </p>
-                  </v-card-subtitle>
-                </router-link>
-              </v-card>
-            </v-col>
+            <Cards :filteredRole="filteredRole" :users="users" />
           </v-row>
         </v-col>
       </v-row>
@@ -123,8 +78,9 @@
 <script>
 import CreateUser from './CreateUser.vue'
 import DeleteUser from './DeleteUser.vue'
+import Cards from './Cards.vue'
 export default {
-  components: { CreateUser, DeleteUser },
+  components: { CreateUser, DeleteUser, Cards },
   data() {
     return {
       items: [
@@ -164,30 +120,8 @@ export default {
 </script>
 
 <style scoped>
-.card__name {
-  color: black;
-  font-weight: bold;
-  font-size: 25px;
-  word-break: keep-all;
-}
-.card__subtitle {
-  font-weight: bold;
-  color: rgb(116, 116, 116);
-  font-size: 20px;
-  margin-bottom: 7px;
-}
-.card__describe {
-  color: black;
-  font-size: 16px;
-}
 .pointer:hover {
   cursor: pointer;
-}
-.card {
-  height: 100%;
-}
-.no-decoration {
-  text-decoration: none;
 }
 .bg-list {
   background-color: rgb(95, 85, 190);
